b2441956ad9ecc4b24a72fef18cac91854e85675
Kevin Chen <kct913@hotmail.com>
2025-06-21 15:12:20 -0700

    wip

--- observations/models.py
+++ observations/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from common.models import TimeStampedModel
+from patients.models import Patient
 
-# Create your models here.
+class Observation(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    metric_code = models.CharField(max_length=20)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    unit = models.CharField(max_length=20)
+    effective_timestamp = models.DateTimeField() # timestamp of when the observation was made
+    device_id = models.CharField(max_length=255, blank=True, null=True)
+    raw_fhir_json = models.JSONField(blank=True, null=True)
+
